2e03ede6ba84b00383c44d388b68417a724184cf
Alex <al.repin.a@yandex.ru>
2022-08-24 21:50:05 +0300

    add Task013

---
+++ Task013/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+string NumberArray = Convert.ToString(number);
+int length = NumberArray.Length;
+
+if (length < 3)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+else
+{
+    Console.WriteLine(NumberArray[2]);
+}
