e47b345b404fef9e7266babef079194c4326a718
Alex <al.repin.a@yandex.ru>
2022-08-24 22:34:10 +0300

    add Task015

---
+++ Task015/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите число:");
+int number = int.Parse(Console.ReadLine());
+
+if (number == 6 || number == 7)
+{
+    Console.WriteLine("Да");
+}
+
+else if (number < 1 || number >7)
+{
+    Console.WriteLine("Нет такого дня");
+}
+else 
+{
+    Console.WriteLine("Нет");
+}
